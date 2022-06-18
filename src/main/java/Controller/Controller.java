be77fac12a04f215f1b27b4338f5739874c8ec83
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-06-18 17:45:13 -0300

    add reponse to form request

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import DAO.ConexaoBD;
+import Model.ContatoBean;
 
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = {"/Controller", "/main", "/insertBD"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
+	ContatoBean contato = new ContatoBean();
 	
     public Controller() {
         super();
@@ -26,11 +28,22 @@ public class Controller extends HttpServlet {
 		case "/main":
 			listarContatos(request, response);
 			break;
+			
+		case "/insertBD":
+			criarContato(request, response);
+			break;
 		}
 	}
 	
 	protected void listarContatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
 			
 		response.sendRedirect("jsp/agenda.jsp");
+	}
+	
+	protected void criarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+
+		contato.setNome(request.getParameter("nome"));
+		contato.setTelefone(request.getParameter("telefone"));
+		contato.setEmail(request.getParameter("email"));
 	}
 }
