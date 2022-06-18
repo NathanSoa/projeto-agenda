6e1be772b6e1c310877a574c84050df3c5264e47
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-06-18 18:49:48 -0300

    add select all method

--- src/main/java/DAO/interfaceDAO.java
+++ src/main/java/DAO/interfaceDAO.java
@@ -1,8 +1,12 @@
 package DAO;
 
+import java.util.ArrayList;
+
 import Model.ContatoBean;
 
 public interface interfaceDAO {
 
 	void insereContato(ContatoBean contato);
+	
+	ArrayList<ContatoBean> retornaTodosContatos();
 }
